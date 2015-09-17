a8ee111f91bd0ff4641f9f5baf921747bc9accb2
COMPPHYS-TTRAN <tttran20@dons.usfca.edu>
2015-09-17 16:28:36 -0700

    asdfkl

--- c_to_f_conversion.py
+++ c_to_f_conversion.py
@@ -1,15 +1,28 @@
-“””
-	
-	>>> conversion(1)
-	33.8
+"""	
+>>> conversion(0)
+32.0
 
-“””
+To run doctests in verbose mode:
+python -m doctest -v c_to_f_conversion.py
 
-def conversion(C):
+"""
+import argparse
+
+def conversion(Cel):
 	
-	F = C * (9./5.) + 32
+	F = Cel * (9./5.) + 32
 	return F
 
 if __name__ == "__main__":
     import doctest
-    doctest.testmod()
+    #doctest.testmod()
+
+    #instantiate an object of the class argparse.ArumentParser()
+    parser = argparse.ArgumentParser() 
+    #Reading argument from command line by invoking the method add_argument()
+    parser.add_argument('-x', type = float)
+    
+    args = parser.parse_args()
+    x = args.x
+
+    print conversion(x) #allows user to input from terminal command line
