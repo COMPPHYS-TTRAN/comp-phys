702e5410d36654a1b12917e3ac03ea523bedd5bc
COMPPHYS-TTRAN <tttran20@dons.usfca.edu>
2015-10-01 18:10:49 -0700

    age

---
+++ whales_basic.py
@@ -0,0 +1,40 @@
+'''
+#HW04
+#Terry Tran
+#Partner: Jacob Baca
+
+'''
+
+import datetime as dt
+import random
+
+class Whale:
+	def __init__(self, name, sex):
+		self.name = name
+		self.sex = sex
+		self.life = dt.datetime.now()
+		print 'A ' + str(self.sex) + ' whale named ' + str(self.name) + ' is born!!!!!! ' + str(dt.datetime.now() - self.life)
+
+	def age(self):
+		self.age = dt.datetime.now() - self.life
+		return self.age
+
+	def __str__(self):
+		return 'A ' + str(self.sex) + ' whale named ' + str(self.name) + ' is born!!!!!! ' + str(dt.datetime.now() - self.life)
+
+	def sing(self):
+		self.sing = sing
+        	print '\a \a \a \a \a'
+
+names = ['RKelly', 'Usher', 'Popo', 'John,', 'Tyrone', 'Albs', 'Drake', 'Scar', 'Simba', 'Tutenkhamun', \
+         'Nala','Kate', 'Orca', 'Lily', 'Tigerlily', 'Wendy', 'Sukhi', 'No', 'TTTT', 'Mina']
+
+names_instance = []
+
+for n in names:
+	bg = ['boy', 'girl']
+	random.shuffle(bg)
+	gbbg = bg[0]
+	x = Whale(n, gbbg)
+	names_instance.append(x)
+
